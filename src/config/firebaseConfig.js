3886dd8d55646d5584b5695e5d758c1319bd6a08
naveed-rana <rana.naveed812@gmail.com>
2019-05-15 13:04:39 +0500

    splitease remove

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,12 +3,7 @@ require("firebase/firestore");
 require("firebase/auth")
 
 let firebaseConfig = {
-  apiKey: "AIzaSyAXZqvYYNIF94TcUX5tir7TH0rWS0c781g",
-  authDomain: "school-project-server.firebaseapp.com",
-  databaseURL: "https://school-project-server.firebaseio.com",
-  projectId: "school-project-server",
-  storageBucket: "school-project-server.appspot.com",
-  messagingSenderId: "303976332687"
+
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
